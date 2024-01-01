73d924278d468fd8703123e78b767f7dedd22755
Matt Koski <maccam912@gmail.com>
2024-01-01 12:37:17 -0600

    Update dependencies and add Prompt model

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.11
 
 WORKDIR /app
 RUN pip3 install -U  torch --index-url https://download.pytorch.org/whl/cpu
-RUN pip3 install -U diffusers transformers sentencepiece accelerate litestar uvicorn
+RUN pip3 install -U diffusers transformers sentencepiece accelerate litestar uvicorn pydantic
 COPY . .
 EXPOSE 8080
 CMD uvicorn main:app --host 0.0.0.0 --port 8080
--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import io
 import torch
 from diffusers import PixArtAlphaPipeline
 from litestar import Litestar, post
+from pydantic import BaseModel
+
+class Prompt(BaseModel):
+    prompt: str
 
 pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float16)
 
 @post("/")
-def generate_image(data: dict[str, str]) -> bytes:
+def generate_image(data: Prompt) -> bytes:
     try:
         print(data)
-        prompt = dict["prompt"]
+        prompt = data.prompt
         print(prompt)
         image = pipe(prompt).images[0]
-        print(image)
+        # print(image)
         buf = io.BytesIO()
-        print(buf)
+        # print(buf)
         image.save(buf, "JPEG")
         buf.seek(0)
         b = buf.read()
         image_b64 = base64.b64encode(b)
-        print(image_b64)
+        # print(image_b64)
         return image_b64
     except Exception as e:
         print(e)
