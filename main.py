c0243302fffca6711cf9bdcfb44307aaee381b7f
Matt Koski <maccam912@gmail.com>
2024-01-01 16:36:26 -0600

    Fix typo in fullgraph parameter

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Prompt(BaseModel):
 # pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", torch_dtype=torch.float32)
 pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-LCM-XL-2-1024-MS", torch_dtype=torch.float32, use_safetensors=True)
 #pipe.text_encoder.to_bettertransformer()
-pipe.transformer = torch.compile(pipe.transformer, mode="reduce-overhead", fullgrah=True)
+pipe.transformer = torch.compile(pipe.transformer, mode="reduce-overhead", fullgraph=True)
 
 @post("/", media_type="image/jpeg")
 def generate_image(data: Prompt) -> bytes:
