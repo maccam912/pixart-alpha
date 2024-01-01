7320fdfcebaf54dead61cdec633f3963bcff0590
Matt Koski <maccam912@gmail.com>
2024-01-01 12:06:59 -0600

    debug

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ pipe = PixArtAlphaPipeline.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", t
 
 @post("/")
 def generate_image(data: dict[str, str]) -> bytes:
-    prompt = dict["prompt"]
-    image = pipe(prompt).images[0]
-    buf = io.BytesIO()
-    image.save(buf, "JPEG")
-    buf.seek(0)
-    b = buf.read()
-    image_b64 = base64.b64encode(b)
-    return image_b64
+    try:
+        print(data)
+        prompt = dict["prompt"]
+        print(prompt)
+        image = pipe(prompt).images[0]
+        print(image)
+        buf = io.BytesIO()
+        print(buf)
+        image.save(buf, "JPEG")
+        buf.seek(0)
+        b = buf.read()
+        image_b64 = base64.b64encode(b)
+        print(image_b64)
+        return image_b64
+    except Exception as e:
+        print(e)
 
 app = Litestar([generate_image])
